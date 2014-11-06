635a94124bbf64b6cff2b2a6db1d9b8909fe41f9
Arne Beer <arne@twobeer.de>
2014-11-06 12:43:06 +0100

    Removed some old code

--- src/systems/particle/particleDrawSystem.lua
+++ src/systems/particle/particleDrawSystem.lua
@@ -3,8 +3,6 @@ ParticleDrawSystem = class("ParticleDrawSystem", System)
 function ParticleDrawSystem:draw()
     for index, entity in pairs(self.targets) do
         love.graphics.draw(entity.components.ParticleComponent.particle, 0, 0)
-        love.graphics.draw(entity.components.ParticleComponent.particle, -love.graphics.getWidth(), 0)
-        love.graphics.draw(entity.components.ParticleComponent.particle, love.graphics.getWidth(), 0)
     end
 end
 
