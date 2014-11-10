640580723e612a14774fddbae802f5ee0ae8ff34
Arne Beer <arne@twobeer.de>
2014-11-11 00:37:50 +0100

    AccelerationSystem and MovementSystem fixed

--- src/systems/physic/accelerationSystem.lua
+++ src/systems/physic/accelerationSystem.lua
@@ -3,17 +3,18 @@ local AccelerationSystem = class("AccelerationSystem", System)
 
 function AccelerationSystem:update(dt)
     for index, entity in pairs(self.targets) do
+        local speedcomp = entity:get("SpeedComponent")
         local speed = entity:get("SpeedComponent")
         local position = entity:get("TransformComponent")
-        if speed.active == true then
-            speed.speed:add(position.direction:multiply(speed.acc*dt))
-            if speed.speed:sum() > speed.maxSpeed then
-                speed.speed:makeUnit():multiply(speed.maxSpeed)
+        if speedcomp.active == true then
+            speed:set(speed:add(position.direction:multiply(speed.acc*dt)))
+            if speed:sum() > maxSpeed then
+                speed:set(speed:getUnit():multiply(speed.maxSpeed))
             end
         else
-            speed.speed = speed.speed:subtract( speed.speed:multiply(-1/3 * dt))
-            if math.abs(speed.speed.x) < 1 then speed.speed.x = 0 end
-            if math.abs(speed.speed.y) < 1 then speed.speed.y = 0 end
+            speed:set(speed:subtract(speed:multiply(-1/3 * dt)))
+            if math.abs(speed.x) < 1 then speed.x = 0 end
+            if math.abs(speed.y) < 1 then speed.y = 0 end
         end
     end
 end
--- src/systems/physic/movementSystem.lua
+++ src/systems/physic/movementSystem.lua
@@ -3,9 +3,9 @@ local MovementSystem = class("MovementSystem", System)
 
 function MovementSystem:update(dt)
     for index, entity in pairs(self.targets) do
-        local speed = entity:get("SpeedComponent")
-        local position = entity:get("TransformComponent")
-        position.position:add(speed.speed:multiply(dt))
+        local speed = entity:get("SpeedComponent").speed
+        local position = entity:get("TransformComponent").position
+        position:add(speed:multiply(dt))
     end
 end
 
