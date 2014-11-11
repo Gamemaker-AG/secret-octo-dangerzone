0be8c8c764c2b214d7e83855509a7d1ba590a4a4
Rafael Epplee <raffomania@gmail.com>
2014-11-11 22:53:37 +0100

    Added constants file

--- src/models/playerModel.lua
+++ src/models/playerModel.lua
@@ -1,5 +1,6 @@
 -- Core
 local Vector = require("helper/vector")
+local constants = require("constants")
 
 -- Graphic components
 local DrawableComponent = require("components/graphic/drawableComponent")
@@ -16,7 +17,9 @@ function PlayerModel:__init()
     self:add(TransformComponent(Vector(100, 100), Vector(1, 0)))
     self:add(SpeedComponent(Vector(0,0), 200, math.pi/180*40, 40, Vector(0,0)))
     local ship = resources.images.circle
-    self:add(DrawableComponent(ship, 0, 1, 1, ship:getWidth()/2, ship:getHeight()/2))
+    local sx, sy = constants.player.width/ship:getWidth(), constants.player.height/ship:getHeight()
+    local ox, oy = ship:getWidth()/2, ship:getHeight()/2
+    self:add(DrawableComponent(ship, 0, sx, sy, ox, oy))
     self:add(ControllableComponent())
     self:add(ZIndex(1))
 end
