68d8d9e141bc3aac1cce08ee1d6e2e7a03d553f3
Rafael Epplee <raffomania@gmail.com>
2014-11-20 20:51:33 +0100

    Removed particle debug code

--- src/systems/particle/particleDrawSystem.lua
+++ src/systems/particle/particleDrawSystem.lua
@@ -2,7 +2,6 @@ local ParticleDrawSystem = class("ParticleDrawSystem", System)
 
 function ParticleDrawSystem:draw()
     for index, entity in pairs(self.targets) do
-        print("motherfucker")
         love.graphics.draw(entity.components.Particle.particle, 0, 0)
     end
 end
--- src/systems/particle/particlePositionSyncSystem.lua
+++ src/systems/particle/particlePositionSyncSystem.lua
@@ -3,7 +3,6 @@ local ParticlePositionSyncSystem = class("ParticlePositionSyncSystem", System)
 function ParticlePositionSyncSystem:update()
     for k, entity in pairs(self.targets) do
         entity:get("Particle").particle:setPosition(entity:get("Transformable").position.x, entity:get("Transformable").position.y)
-        print(entity:get("Transformable").position.x .. "  " .. entity:get("Transformable").position.y)
     end
 end
 
