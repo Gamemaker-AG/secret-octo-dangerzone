6c212687b358fad31fdf0ab6844705b4b7ba1f29
Arne Beer <arne@twobeer.de>
2015-01-13 21:19:51 +0100

    Lovetoys update, ParticlePositionSyncSystem refactored

--- src/systems/particle/ParticlePositionSyncSystem.lua
+++ src/systems/particle/ParticlePositionSyncSystem.lua
@@ -2,18 +2,15 @@ local ParticlePositionSyncSystem = class("ParticlePositionSyncSystem", System)
 
 function ParticlePositionSyncSystem:update()
     for k, entity in pairs(self.targets) do
-        local particle = entity:get("Particle")
-        local transformable = entity:get("Transformable")
-        local radian = transformable.direction:getRadian()
-        local rotatedOffset = particle.offset:rotate(transformable.direction:getRadian()):add(entity:get("Transformable").position)
-
-        particle.particle:setPosition(rotatedOffset.x, rotatedOffset.y)
-        particle.particle:setDirection(math.pi+radian)
+        self:syncPosition(entity)
     end
 end
 
 function ParticlePositionSyncSystem:updatePosition(event)
-    local entity = event.entity
+    self:syncPosition(event.entity)
+end
+
+function ParticlePositionSyncSystem:syncPosition(entity)
     local particle = entity:get("Particle")
     local transformable = entity:get("Transformable")
     local radian = transformable.direction:getRadian()
@@ -22,7 +19,6 @@ function ParticlePositionSyncSystem:updatePosition(event)
     particle.particle:setPosition(rotatedOffset.x, rotatedOffset.y)
     particle.particle:setDirection(math.pi+radian)
 end
-
 
 function ParticlePositionSyncSystem:requires()
     return {"Particle", "Transformable"}
