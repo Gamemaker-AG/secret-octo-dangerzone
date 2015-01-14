db6750c5b2847e1e55249018260d499ae23d1e47
Anne-Victoria Meyer <2meyer@informatik.uni-hamburg.de>
2015-01-14 19:31:53 +0100

    shield looks better now

--- src/systems/gameplay/ShieldSystem.lua
+++ src/systems/gameplay/ShieldSystem.lua
@@ -12,9 +12,25 @@ function ShieldSystem:draw()
     for _, entity in pairs(self.targets) do
         local shield = entity:get("Shield")
         local position = entity:get("Transformable").position
-        love.graphics.setColor(200, 100, 250, 50)
-        if shield.hitpoints > 20 then love.graphics.circle("fill", position.x, position.y, 90, 8) end
-        if shield.hitpoints > 100 then love.graphics.circle("fill", position.x, position.y, 100, 8) end
+        local shieldPercentage = 100*shield.hitpoints/shield.maxHitpoints
+        local maxAlpha = 40
+        local minAlpha = 40
+        if (shieldPercentage/100)*maxAlpha < minAlpha then alpha = minAlpha
+        else alpha = (shieldPercentage/100)*maxAlpha
+        end
+        love.graphics.setColor(200, 100, 250, alpha)
+        if shieldPercentage > 0 then love.graphics.circle("fill", position.x, position.y, 85, 8) end
+        love.graphics.setColor(200, 100, 250, 2*alpha)
+        if shieldPercentage > 0 then love.graphics.circle("line", position.x, position.y, 85, 8) end
+        love.graphics.setColor(200, 100, 250, 5*alpha/6)
+        if shieldPercentage > 20 then love.graphics.circle("fill", position.x, position.y, 95, 8) end
+        love.graphics.setColor(200, 100, 250, 4*alpha/6)
+        if shieldPercentage > 40 then love.graphics.circle("fill", position.x, position.y, 105, 8) end
+        love.graphics.setColor(200, 100, 250, 3*alpha/6)
+        if shieldPercentage > 60 then love.graphics.circle("fill", position.x, position.y, 115, 8) end
+        love.graphics.setColor(200, 100, 250, 2*alpha/6)
+        if shieldPercentage > 80 then love.graphics.circle("fill", position.x, position.y, 125, 8) end
+
     end
 end
 
