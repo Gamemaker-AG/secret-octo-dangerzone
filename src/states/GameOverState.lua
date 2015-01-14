c98a1ee9a069e484dc343fc6b0d20cf95cb7fd7a
Arne Beer <arne@twobeer.de>
2015-01-14 20:12:58 +0100

    GameOverState fixed

--- src/states/GameOverState.lua
+++ src/states/GameOverState.lua
@@ -7,6 +7,7 @@ local GameOverState = class("GameOverState", State)
 function GameOverState:load()
     self.engine = Engine()
     self.eventmanager = EventManager()
+    self.eventmanager:addListener("KeyPressed", {self, self.keypressed})
 end
 
 function GameOverState:update(dt)
@@ -15,14 +16,11 @@ end
 
 function GameOverState:draw()
     self.engine:draw()
-
+    love.graphics.setColor(255, 255, 255, 255)
     love.graphics.print("Press any button to restart the game", 10, 10)
 end
 
-function GameOverState:keypressed(key, isrepeat)
-    self.eventmanager:fireEvent(KeyPressed(key, isrepeat))
-
-    -- Start the game when any key is pressed
+function GameOverState:keypressed()
     stack:popload()
 end
 
