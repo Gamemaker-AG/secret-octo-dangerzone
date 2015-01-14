2bfd719a80c66b3f1ef3972eb386ae91a5da8c82
Rafael Epplee <raffomania@gmail.com>
2015-01-14 19:40:17 +0100

    Don't rotate turret buttons

--- src/models/ui/createButtonComponents.lua
+++ src/models/ui/createButtonComponents.lua
@@ -5,13 +5,13 @@ local Diameter = require("components/physic/Diameter")
 
 local function createButtonComponents(fn, img, offset, diameter)
     local comps = {}
-    
+
     table.insert(comps, Clickable(fn))
 
     local img = img or resources.images.circle
     table.insert(comps, Drawable(img, 500, nil, nil, img:getWidth()/2, img:getHeight()/2))
 
-    table.insert(comps, Transformable(offset))
+    table.insert(comps, Transformable(offset, nil, false))
     table.insert(comps, Diameter(diameter or img:getWidth()))
 
     return comps
