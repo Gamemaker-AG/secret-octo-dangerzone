57be6dd199b19edb25d41f0d0b34abea6b162b75
Anne-Victoria Meyer <2meyer@informatik.uni-hamburg.de>
2015-03-20 21:31:39 +0100

    Spawn loot

--- src/systems/gameplay/GoldSystem.lua
+++ src/systems/gameplay/GoldSystem.lua
@@ -1,13 +1,17 @@
+local Transformable = require("components/physic/Transformable")
 local GoldSystem = class("GoldSystem", System)
-
 
 function GoldSystem:addGold(event)
     local enemy = event.enemy
 	local amount = enemy:get("DropsGold").gold
-	for _, entity in pairs(self.targets) do
-		local playergold = entity:get("HasGold")
-		playergold.gold = playergold.gold + amount
-	end
+	local player = table.firstElement(stack:current().engine:getEntitiesWithComponent("Player"))
+	local playergold = player:get("HasGold")
+	playergold.gold = playergold.gold + amount
+
+	local loot = Entity()
+	loot:add(Transformable(enemy:get("Transformable").offset:clone()))
+	loot:add(Drawable(resources.images.circle))
+	stack:current().engine:addEntity(loot)
 end
 
 function GoldSystem:requires()
