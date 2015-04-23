c40497f075f806c7f80f9ee1043d4dd2bbf57770
Arne Beer <arne@twobeer.de>
2015-04-23 23:30:27 +0200

    First debugging of star generation

--- src/generators/stars.lua
+++ src/generators/stars.lua
@@ -8,37 +8,46 @@ return function(position, size)
     local starPositions = {}
     local subTileSize = size/4
 
-    local tempCanvas = love.graphics.newCanvas(size*3/4, size*3/4)
+    -- Create and set canvas for first drawing
+    local tempCanvas = love.graphics.newCanvas(subTileSize*6, subTileSize*6)
     love.graphics.setCanvas(tempCanvas) 
 
-    for i=0, 5, 1 do
-        for j=0, 5, 1 do
+    
+    for x=0, 5, 1 do
+        for y=0, 5, 1 do
             local starPos = {}
             local subTile = {}
-            subTile.x = position.x-subTileSize+subTileSize*i
-            subTile.y = position.y-subTileSize+subTileSize*j
+            -- Define subTile Position
+            subTile.x = position.x-subTileSize+subTileSize*x
+            subTile.y = position.y-subTileSize+subTileSize*y
+
+            -- Get seed from subTile Position
             math.randomseed(2 * subTile.y + 2 * subTile.y * subTile.x - subTile.x + 2 * subTile.y)
+            -- Draw Stars
             for k=0, 2, 1 do
-                love.graphics.draw(resources.images.circle, math.random(0, subTileSize), math.random(0, subTileSize))
+                love.graphics.draw(resources.images.circle, subTile.x + math.random(0, subTileSize), subTile.y + math.random(0, subTileSize))
             end
         end
     end
 
-    local quad = love.graphics.newQuad(subTileSize, subTileSize, size, size, 1, 1)
 
+    -- Create quad to cut off unneeded buffer
+    local quad = love.graphics.newQuad(subTileSize, subTileSize, size, size, 1, 1)
+    -- Create final buffer
     local tempCanvas2 = love.graphics.newCanvas(size, size)
     love.graphics.setCanvas(tempCanvas2)
-    love.graphics.draw(love.graphics.newImage(tempCanvas:getImageData()), quad, 0, 0)
-    local finishedTile = love.graphics.newImage(tempCanvas2:getImageData())
-    
 
+    -- Cut off edges
+    love.graphics.draw(tempCanvas, quad, 0, 0)
+
+    -- Restore standard canvas
     love.graphics.setCanvas() 
 
-	local star = Entity()
-	star:add(Transformable(position:clone():add(size/2)))
-	star:add(Drawable(finishedTile))
-	star:add(Circle(percentToPixels(1)))
-	star:add(Debris())
-	stack:current().engine:addEntity(star)
-	return star
+    local star = Entity()
+    star:add(Transformable(position:clone():add(size/2)))
+    star:add(Drawable(tempCanvas2))
+    star:add(Circle(percentToPixels(1)))
+    star:add(Debris())
+    stack:current().engine:addEntity(star)
+    return star
 end
