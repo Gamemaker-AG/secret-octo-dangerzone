cbeab6342d148559b0892e357ce9e608b5c4bf81
Rafael Epplee <rafael.epplee@sinnerschrader.com>
2015-05-05 22:00:00 +0200

    Fix indentation

--- src/systems/gameplay/DestroySystem.lua
+++ src/systems/gameplay/DestroySystem.lua
@@ -31,34 +31,34 @@ function DestroySystem:update(dt)
         explosion:add(particleComponent)
         local particle = particleComponent.particle
     
-            -- Setzen der Position
-            local transformable = entity:get("Transformable")
-            local radian = transformable.direction:getRadian()
-            local rotatedOffset = particleComponent.offset:rotate(transformable.direction:getRadian()):add(transformable.position)
-            local mass
-            if entity:has("Pirate") then
-                mass = 20
-            else
-                mass = 1
-            end
+        -- Setzen der Position
+        local transformable = entity:get("Transformable")
+        local radian = transformable.direction:getRadian()
+        local rotatedOffset = particleComponent.offset:rotate(transformable.direction:getRadian()):add(transformable.position)
+        local mass
+        if entity:has("Pirate") then
+            mass = 20
+        else
+            mass = 1
+        end
 
-            local relation = 1/mass
-            particle:setPosition(rotatedOffset.x, rotatedOffset.y)
-            particle:setEmissionRate(mass*8)
-            particle:setSpeed(mass*3, mass*3)
-            particle:setSizes(2.5, 2.8)
-            particle:setColors(255, 255, 255, 255,
-                        255, 255, 0, 255,
-                        200, 0, 0, 255,
-                        255, 100, 0, 155)
-            particle:setEmitterLifetime(0.6) -- Zeit die der Partikelstrahl anhält
-            particle:setParticleLifetime(0.5, 0.6) -- setzt Lebenszeit in min-max
-            -- particle:setOffset(x, y) -- Punkt um den der Partikel rotiert
-            particle:setRotation(0, 360) -- Der Rotationswert des Partikels bei seiner Erstellung
-            particle:setDirection(100)
-            particle:setSpread(360)
-            particle:setRadialAcceleration((mass*-7.5)*relation, (mass*-7.5)*relation)
-            particle:start()
+        local relation = 1/mass
+        particle:setPosition(rotatedOffset.x, rotatedOffset.y)
+        particle:setEmissionRate(mass*8)
+        particle:setSpeed(mass*3, mass*3)
+        particle:setSizes(2.5, 2.8)
+        particle:setColors(255, 255, 255, 255,
+                    255, 255, 0, 255,
+                    200, 0, 0, 255,
+                    255, 100, 0, 155)
+        particle:setEmitterLifetime(0.6) -- Zeit die der Partikelstrahl anhält
+        particle:setParticleLifetime(0.5, 0.6) -- setzt Lebenszeit in min-max
+        -- particle:setOffset(x, y) -- Punkt um den der Partikel rotiert
+        particle:setRotation(0, 360) -- Der Rotationswert des Partikels bei seiner Erstellung
+        particle:setDirection(100)
+        particle:setSpread(360)
+        particle:setRadialAcceleration((mass*-7.5)*relation, (mass*-7.5)*relation)
+        particle:start()
 
         stack:current().engine:addEntity(explosion)
 
